f31fc3437d53cdaf8a2bfe9b342e43a442366663
Lloyd Polley <lloydpolley@googlemail.com>
2025-10-02 16:29:59 +0100

    rebuild

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import LoginForm from "../../../components/Forms/LoginForm/LoginForm";
+import LoginForm from "@/components/Forms/LoginForm/LoginForm";
 
 export default async function Login() {
   return <LoginForm />;
